3a0418b61e5f8fe3764cd5f3def5fa286b186153
thanhdhyk117 <63649787+thanhdhyk117@users.noreply.github.com>
2022-12-19 04:07:23 +0700

    delete file

--- chuc_nang/quang_cao/trai.php
+++
@@ -1,9 +0,0 @@
-<br>Quảng cáo <br><br>
-<?php
-include "ket_noi.php";
-	$tv="select html from quang_cao where vi_tri='trai' ";
-	$tv_1=mysqli_query($conn, $tv);
-	$tv_2=mysqli_fetch_array($tv_1);
-	echo $tv_2['html'];
-mysqli_close($conn);
-?>
--- chuc_nang/tim_kiem/vung_tim_kiem.php
+++
@@ -1,9 +0,0 @@
-
-<form  >
-	
-    <input type="hidden" name="thamso" value="tim_kiem" >
-    <input type="text" name="tu_khoa" value="" placeholder="Nhập tên sản phẩm" style=" color:#ccc;" >
-    
-    <button type="submit">Seach</button>
-</form> 
-
